a8bcf5b833dcb7ce19a66f6a0b03ae0c271a3ee1
sebaschauer <122497785+sebaschauer@users.noreply.github.com>
2024-04-27 11:14:09 -0300

    Clase 01
    Se agrego lo visto en clase 1

---
+++ 01.Clase/PrimerApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string mensaje = "Hola Mundo soy Seba y estoy trabjando en mi primer proyecto en C#";
+string mensaje2 = "Probando algunas boludeces y tratando de no cagarla toda jejej";
+Console.WriteLine(mensaje);
+Console.WriteLine(mensaje2);
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Curso-C-Sharp-Coderhouse
+
+
+
+# Repositorio con los proyectos y ejemplos vistos en clase del curso de C# de Coderhouse, comision 58425
